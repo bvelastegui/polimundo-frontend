01526191e9527a7bb206785c7cb28fdbaac2a9a5
Bryan Velastegui <bvelastegui@intisana.com>
2022-03-24 16:11:16 -0500

    [WIP] Búsqueda Se crea el componente de búsqueda

---
+++ src/components/SearchFlight.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card class="px-4 py-3">
+    <v-card-title>Encuentra tu vuelo</v-card-title>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col
+              cols="12"
+              md="6"
+              lg="3"
+          >
+            <v-text-field
+                color="#3A1C71"
+                filled
+                label="Origen"
+            ></v-text-field>
+          </v-col>
+          <v-col
+              cols="12"
+              md="6"
+              lg="3"
+          >
+            <v-text-field
+                color="#3A1C71"
+                filled
+                label="Destino"
+            ></v-text-field>
+          </v-col>
+          <v-col
+              cols="12"
+              md="6"
+              lg="3"
+          >
+            <v-text-field
+                color="#3A1C71"
+                filled
+                append-icon="mdi-calendar"
+                label="Fecha de partida"
+            ></v-text-field>
+          </v-col>
+          <v-col
+              cols="12"
+              md="6"
+              lg="3"
+          >
+            <v-text-field
+                color="#3A1C71"
+                filled
+                append-icon="mdi-calendar"
+                label="Fecha de retorno"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+              cols="12"
+              md="3"
+              offset-md="9"
+              lg="2"
+              offset-lg="10"
+          >
+            <v-fab-transition>
+              <v-btn
+                  block
+                  dark
+                  color="#3A1C71"
+                  elevation="2"
+              >
+                Buscar
+              </v-btn>
+            </v-fab-transition>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-card>
+</template>
