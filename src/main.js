3f072b23874cd517f318436cfec7c890c21d6def
Bryan Velastegui <bvelastegui@intisana.com>
2022-03-25 11:42:45 -0500

    Vuex store Se agrega el store con el plugin de log a la aplicación

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue"
 import App from "./App.vue"
 import vuetify from "./plugins/vietify"
-import router from "./router";
+import router from "./router"
+import store from "./store"
 
 new Vue({
     vuetify,
     router,
+    store,
     render: (h) => h(App)
 }).$mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,12 @@
-// TODO
+import Vue from 'vue'
+import Vuex, {createLogger} from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    plugins: [
+        createLogger()
+    ]
+})
+
+export default store
