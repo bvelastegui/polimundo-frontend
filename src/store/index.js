7ccfa468eac46c3541e95f66b26cdd8478113606
Bryan Velastegui <bvelastegui@intisana.com>
2022-03-25 13:24:14 -0500

    Se agrega el módulo de vuelos en vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex, {createLogger} from 'vuex'
 import search from "./modules/search";
+import flights from "./modules/flights";
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     modules: {
-        search
+        search,
+        flights
     },
     plugins: [
         createLogger()
---
+++ src/store/modules/flights.js
@@ -0,0 +1,38 @@
+import flights from "../../../api/flights";
+
+const state = () => ({
+    items: [],
+    loading: false,
+})
+
+const getters = {}
+
+const actions = {
+    async searchFlights({commit}) {
+        commit('loading', true)
+        const data = await flights.find()
+        commit('setItems', data)
+        commit('loading', false)
+    },
+
+    clearItems({commit}) {
+        commit('setItems', [])
+    }
+}
+
+const mutations = {
+    setItems(state, data) {
+        state.items = data
+    },
+    loading(state, value) {
+        state.loading = value
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
